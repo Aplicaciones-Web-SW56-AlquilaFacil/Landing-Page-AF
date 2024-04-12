de4289110f715529fc0151839b508cf70a1bfb09
Tato <italoluna23@gmail.com>
2024-04-12 00:38:35 -0500

    chore: Added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#D13333',
+      },
+    },
   },
   plugins: [],
 }
